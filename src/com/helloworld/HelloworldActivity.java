6948f2149cc3ceb35495933553ec911fadf01a20
oras <orasnin@gmail.com>
2011-11-29 19:01:49 +0200

    test7

---
+++ src/com/helloworld/HelloworldActivity.java
@@ -0,0 +1,24 @@
+package com.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HelloworldActivity extends Activity implements View.OnClickListener {
+	Button button;
+	int touchCount;
+	
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		button = new Button(this);
+		button.setText("Touch me!");
+		button.setOnClickListener(this);
+		setContentView(button);
+	}
+	
+	public void onClick(View v){
+		touchCount++;
+		button.setText("Touch me " + touchCount + " time(s)");
+	}
+}
