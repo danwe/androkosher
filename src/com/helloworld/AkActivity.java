192173bfa7a5b884814632ede4868a62c2aad29b
oras <orasnin@gmail.com>
2011-11-29 16:14:57 +0200

    Test4

--- src/com/helloworld/AkActivity.java
+++ src/com/helloworld/AkActivity.java
@@ -2,12 +2,24 @@ package com.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AkActivity extends Activity {
+public class AkActivity extends Activity implements View.OnClickListener{
+	Button button;
+	int touchCount;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        button.setText("Touch me!");
+        button.setOnClickListener(this);
         setContentView(R.layout.main);
+    }
+    
+    public void onClick(View v){
+    	touchCount++;
+    	button.setText("Touch me "+ touchCount + " time(s)");
     }
 }
