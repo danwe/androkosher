986578f02fbfeb7632b758884c3c2452d86e4f6f
danweill <danweill@danweill-PC>
2011-12-19 05:44:37 +0200

    WorkWithNewPage

--- src/com/androk/AndroKActivity.java
+++ src/com/androk/AndroKActivity.java
@@ -1,13 +1,39 @@
 package com.androk;
 
+
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class AndroKActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+import android.location.Location;
+
+import android.widget.Button;
+
+public class AndroKActivity extends Activity implements OnClickListener
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		View loginButton=findViewById(R.id.login_button);
+		loginButton.setOnClickListener(this);
+			
+	}
+	
+
+	public void onClick(View v)
+	{
+		Intent i = new Intent(this, login.class);
+		startActivity(i);
+	
+	}
 }
+
+
+
