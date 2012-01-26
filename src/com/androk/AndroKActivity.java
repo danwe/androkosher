60f1c457c07ce1f622c34258edefa4ed555d498a
oras <oras@oras-PC>
2012-01-26 23:06:31 +0200

    Login Set

--- src/com/androk/AndroKActivity.java
+++ src/com/androk/AndroKActivity.java
@@ -1,37 +1,59 @@
+/*This class was written by: Or Asnin
+ * Login to application.
+*/
+
 package com.androk;
-
-
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
-
-
-import android.location.Location;
-
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AndroKActivity extends Activity implements OnClickListener
 {
+	public Login login;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
+		login = new Login();
+		
 		View loginButton=findViewById(R.id.login_button);
 		loginButton.setOnClickListener(this);
-			
 	}
 	
 
 	public void onClick(View v)
 	{
+		EditText userTx = (EditText)findViewById(R.id.editText2);
+		String user = userTx.getText().toString();
 		
-		Intent i = new Intent(this, login.class);
-		startActivity(i);
+		EditText passwordTx = (EditText)findViewById(R.id.editText1);
+		String password = passwordTx.getText().toString();
+		
+		Toast.makeText(this,"Connecting...",Toast.LENGTH_LONG).show();
+	
+		try {
+				if(login.executeHttpGet(user.toString(), password.toString())){
+					Toast.makeText(this,"Authentication Granted",Toast.LENGTH_LONG).show();
+					Intent i = new Intent(this, Menu.class);
+					startActivity(i);
+			}
+			else
+				Toast.makeText(this,"Authentication failed!",Toast.LENGTH_LONG).show();
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	
 	}
 	
--- src/com/androk/GetData.java
+++ src/com/androk/GetData.java
@@ -1,77 +1,52 @@
 package com.androk;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import android.app.ListActivity;
-import android.net.ParseException;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
-public class GetData extends ListActivity {
-JSONArray jArray;
-String result = null;
-InputStream is = null;
-StringBuilder sb=null;
-@Override
-public void onCreate(Bundle savedInstanceState) {
-super.onCreate(savedInstanceState);
-setContentView(R.layout.main);
- ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-//http post
-try{
-     HttpClient httpclient = new DefaultHttpClient();
-     HttpPost httppost = new HttpPost("androkosher.x10.mx/follow.php?one='A'");
-     httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-     HttpResponse response = httpclient.execute(httppost);
-     HttpEntity entity = response.getEntity();
-     is = entity.getContent();
-     }catch(Exception e){
-         Log.e("log_tag", "Error in http connection"+e.toString());
-    }
-//convert response to string
-try{
-      BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
-       sb = new StringBuilder();
-       sb.append(reader.readLine() + "\n");
-       String line="0";
-       while ((line = reader.readLine()) != null) {
-                      sb.append(line + "\n");
-        }
-        is.close();
-        result=sb.toString();
-        }catch(Exception e){
-              Log.e("log_tag", "Error converting result "+e.toString());
-        }
-//paring data
-int ct_id;
-String ct_name;
-try{
-      jArray = new JSONArray(result);
-      JSONObject json_data=null;
-      for(int i=0;i<jArray.length();i++){
-             json_data = jArray.getJSONObject(i);
-             ct_id=json_data.getInt("CITY_ID");
-             ct_name=json_data.getString("CITY_NAME");
-         }
-      }
-      catch(JSONException e1){
-    	  Toast.makeText(getBaseContext(), "No City Found" ,Toast.LENGTH_LONG).show();
-      } catch (ParseException e1) {
-			e1.printStackTrace();
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class GetData {
+	
+	public String [] executeHttpGet() throws Exception {
+		BufferedReader in = null;
+		
+			try {
+				HttpClient client = new DefaultHttpClient();
+				HttpGet request = new HttpGet();
+				request.setURI(new URI("http://androkosher.x10.mx/get.php"));
+				HttpResponse response = client.execute(request);
+				in = new BufferedReader
+						(new InputStreamReader(response.getEntity()
+								.getContent()));
+				StringBuffer sb = new StringBuffer("");
+				String line = "";
+				String NL = System.getProperty("line.separator");
+				
+				while ((line = in.readLine()) != null) {
+					sb.append(line + NL);
+				}
+				
+				in.close();
+				
+				String [] splitted = (sb.toString()).split("<br/>");
+				
+				return splitted;
+				} finally {
+					if (in != null) {
+						try {
+							in.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			
+		}
 }
-}
-    
+					
+			
