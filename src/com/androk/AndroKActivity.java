fd55181d75647c9fde7023c5762384c7f7cd1ff3
oras <oras@oras-PC>
2012-01-29 09:17:54 +0200

    Login Stuck bug fixed

--- src/com/androk/AndroKActivity.java
+++ src/com/androk/AndroKActivity.java
@@ -20,7 +20,7 @@ public class AndroKActivity extends Activity implements OnClickListener
 	public void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.premiss);
+		//setContentView(R.layout.premiss);
 		
 		setContentView(R.layout.main);
 		
